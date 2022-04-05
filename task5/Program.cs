8a5ae7b2964cc62f05b85b4b188abf880380f3d2
Dasha <steshitc@mail.ru>
2022-04-05 16:28:27 +0300

    add task

---
+++ task5/Program.cs
@@ -0,0 +1,18 @@
+﻿//5) Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+	
+public class Task5
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Введи координату X, (Х!=0): ");
+		int x = int.Parse(Console.ReadLine());
+		Console.WriteLine("Введи координату Y, (Y!=0): ");
+		int y = int.Parse(Console.ReadLine());
+		
+		if(x > 0 && y > 0) Console.WriteLine("Первая четверть");
+		else if(x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+		else if(x < 0 && y < 0) Console.WriteLine("Третья четверть");
+		else if(x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+	}
+}	
+
---
+++ task6/Program.cs
@@ -0,0 +1,63 @@
+﻿//6) Найти расстояние между точками в пространстве 2D/3D
+public class Task6
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Введи пространство (2D или 3D): "); 
+		string d = Console.ReadLine();
+
+        int x1, y1, x2, y2;
+        double distance;
+
+		switch(d)
+		{
+		    case "2D":
+			    Console.WriteLine("Введи координату Х точки 1: ");
+			    x1 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Y точки 1: ");
+			    y1 = int.Parse(Console.ReadLine());	
+			
+			    Console.WriteLine("Введи координату Х точки 2: ");
+			    x2 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Y точки 2: ");
+			    y2 = int.Parse(Console.ReadLine());
+	
+			    distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)); 
+			
+			    Console.WriteLine("Расстояние: " + distance);
+	
+            break;	
+
+            case "3D":
+			    Console.WriteLine("Введи координату Х точки 1: ");
+			    x1 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Y точки 1: ");
+			    y1 = int.Parse(Console.ReadLine());	
+			
+			    Console.WriteLine("Введи координату Z точки 1: ");
+			    int z1 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Х точки 2: ");
+			    x2 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Y точки 2: ");
+			    y2 = int.Parse(Console.ReadLine());
+			
+			    Console.WriteLine("Введи координату Z точки 2: ");
+			    int z2 = int.Parse(Console.ReadLine());
+	
+			    distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)); 
+			
+			    Console.WriteLine("Расстояние: " + distance);
+
+            break;	
+        default: Console.WriteLine("Вы ввели неверный символ"); 
+        break;
+        }
+
+	}
+}
+
